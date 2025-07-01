eddb1b5f163ecf8688a39cb5477351632d8af277
Mark Charles <me@markcharles.dev>
2025-07-01 21:59:24 +0100

    tidy up

--- src-tauri/src/commands/products/mod.rs
+++ src-tauri/src/commands/products/mod.rs
@@ -1,8 +1,7 @@
 use crate::autonomi::access::client;
-use crate::autonomi::products::{get_product_by_id, get_product_list};
+use crate::autonomi::products::{get_product_by_id, get_product_list, ALL_PRODUCTS_PTR};
 use crate::commands::products::models::Product;
 use crate::state::AppState;
-use alloy_primitives::address;
 use autonomi::PointerAddress;
 use std::sync::Mutex;
 use tauri::State;
@@ -35,8 +34,7 @@ pub async fn get_all_products(
     let network = &state.lock().unwrap().network.clone();
     let client = client(network).await;
 
-    let product_list_ptr = "974c445dc953ff66e273de57acbc067bdfd3610418b8a9a1fef73560da96ec4e1ca1060e8737fe681ebbf6081980a60d";
-    let pointer_address = PointerAddress::from_hex(product_list_ptr).unwrap();
+    let pointer_address = PointerAddress::from_hex(ALL_PRODUCTS_PTR).unwrap();
 
     get_product_list(&client, &pointer_address).await
 }
